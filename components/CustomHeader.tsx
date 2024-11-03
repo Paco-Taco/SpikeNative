86b5871d74d6b81cc7d2678fa3cb087de7c54ba2
Francisco Rodriguez <chillguy27@outlook.com>
2024-11-03 12:43:26 -0600

    initial commit & header implemented

---
+++ components/CustomHeader.tsx
@@ -0,0 +1,153 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { ColorPalette, Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.searchIcon}
+          name="search-outline"
+          size={20}
+          color={ColorPalette.medium}
+        />
+        <TextInput
+          placeholder="Veterinaries, care services, products"
+          style={styles.input}
+          placeholderTextColor={ColorPalette.medium}
+          cursorColor={ColorPalette.yellowPalette}
+        />
+      </View>
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons
+            name="options-outline"
+            size={20}
+            color={ColorPalette.primary}
+          ></Ionicons>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Ionicons
+            name="location-outline"
+            color={ColorPalette.medium}
+            size={20}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.titleContainer}>
+          <Text style={styles.title}>Clinics on</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>Manzanillo</Text>
+            <Ionicons
+              name="chevron-down-outline"
+              size={20}
+              color={ColorPalette.primary}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.profileButton}>
+          <Image
+            style={styles.profileButton}
+            source={require("@/assets/images/ic_pet.jpg")}
+          />
+        </TouchableOpacity>
+      </View>
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: ColorPalette.darkGrayPalette,
+    flex: 1,
+  },
+  container: {
+    height: 60,
+    flexDirection: "row",
+    gap: 20,
+    backgroundColor: ColorPalette.darkGrayPalette,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  leftIcon: {
+    width: 30,
+    height: 200,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.dark.text,
+  },
+  profileButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: ColorPalette.lightGrey,
+  },
+  locationName: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: ColorPalette.darkGrayPalette,
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10 /* propio */,
+    alignItems: "center",
+  },
+  searchField: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: ColorPalette.graphitePalette,
+    borderRadius: 8,
+  },
+  input: {
+    padding: 10,
+    color: ColorPalette.lightGrey,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+});
+
+export default CustomHeader;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,37 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const ColorPalette = {
+  bluePalette: "#1368CC",
+  darkGrayPalette: "#2B3240",
+  graphitePalette: "#39434F",
+  yellowPalette: "#FFC542",
+  primary: "#1368CC",
+  lightGrey: "#FCF8FF",
+  grey: "#EEE9F0",
+  medium: "#9F9AA1",
+  mediumDark: "#424242",
+  green: "#437919",
+};
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
 };
