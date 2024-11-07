8e298fcfc3e8aea4848eb51d80a8366d00cb94c5
Francisco Rodriguez <chillguy27@outlook.com>
2024-11-07 11:19:12 -0600

    veterinary card design wip

--- app/(app)/(user)/index.tsx
+++ app/(app)/(user)/index.tsx
@@ -1,101 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Card, Text, Button } from "react-native-ui-lib";
+import { View, Card, Text, Button, Image } from "react-native-ui-lib";
 import { FlatList } from "react-native";
 import { ColorPalette } from "@/constants/Colors";
 import { useUserStore } from "@/stores/user.store";
-
-const veterinaryClinics = [
-  {
-    id: "1",
-    name: "Veterinaria San Paws",
-    address: "123 Calle Feliz",
-    contact: "555-1234",
-  },
-  {
-    id: "2",
-    name: "Clínica Animalia",
-    address: "456 Avenida Verde",
-    contact: "555-5678",
-  },
-  {
-    id: "3",
-    name: "Pets Care Center",
-    address: "789 Camino Salud",
-    contact: "555-9876",
-  },
-  {
-    id: "4",
-    name: "Amigos Peludos",
-    address: "101 Parque Norte",
-    contact: "555-3456",
-  },
-  {
-    id: "5",
-    name: "Huellas Felices",
-    address: "202 Zona Central",
-    contact: "555-6543",
-  },
-];
+import { Veterinary } from "@/types/userTypes.types";
+import CardVeterinary from "@/components/CardVeterinary";
 
 const Index = () => {
-  const {getVets} = useUserStore(state => state)
+  const { getVets } = useUserStore((state) => state);
+  const [veterinaryClinics, setVeterinaryClinics] = useState<Veterinary[]>([]);
 
-  const renderItem = ({ item }) => (
-    <Card
-      marginV-10
-      padding-20
-      borderRadius={10}
-      backgroundColor={ColorPalette.lightGraphite}
-    >
-      <Text text50 color={ColorPalette.grey}>
-        {item.name}
-      </Text>
-      <Text text70 color={ColorPalette.grey}>
-        {item.address}
-      </Text>
-      <Text text80 color={ColorPalette.grey}>
-        {item.contact}
-      </Text>
-      {/* <Link href={`/veterinaryDetails/${item.id}`} asChild> */}
-      <Button
-        label="Ver detalles"
-        marginT-10
-        backgroundColor={ColorPalette.bluePalette}
-      />
-      {/* </Link> */}
-    </Card>
+  const renderItem = ({ item }: { item: Veterinary }) => (
+    <CardVeterinary item={item} />
   );
 
   useEffect(() => {
     const fetchVets = async () => {
-      const result = await getVets()
-      console.log('Resultado useEffect: ',result)
-    }
-    fetchVets()
-  }, [])
+      const result = await getVets();
+      setVeterinaryClinics(result ? result.veterinaries : []);
+    };
+    fetchVets();
+  }, []);
 
   return (
     <View
       flex
-      useSafeArea
-      paddingT-120
-      paddingH-20
+      marginT-100
       backgroundColor={ColorPalette.graphitePalette}
+      // useSafeArea
     >
-      {/* <Text text40 marginB-20>
-        Veterinarias
-      </Text> */}
-      <FlatList
-        data={veterinaryClinics}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-        contentContainerStyle={{ paddingBottom: 20 }}
-      />
-      <Link href={"/petRegister"} asChild>
+      <View paddingH-20>
+        <FlatList
+          data={veterinaryClinics}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+        {/* <Link href={"/petRegister"} asChild>
         <Button label="Pet register" />
-      </Link>
+      </Link> */}
+      </View>
     </View>
   );
 };
---
+++ components/CardVeterinary.tsx
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Card, Image, Text, View } from "react-native-ui-lib";
+import { Ionicons } from "@expo/vector-icons";
+import { Veterinary } from "@/types/userTypes.types";
+import { ColorPalette } from "@/constants/Colors";
+
+const CardVeterinary = ({ item }: { item: Veterinary }) => {
+  return (
+    <Card
+      marginV-40
+      padding-20
+      borderRadius={10}
+      backgroundColor={ColorPalette.lightGraphite}
+      style={styles.cardStyles}
+      onPress={() => {}}
+    >
+      <Image
+        source={{ uri: item.img }}
+        style={{ height: 110, width: "30%", borderRadius: 10 }}
+      />
+
+      <View style={styles.infoAreaStyles}>
+        <Text text60 marginB-5 color={ColorPalette.grey}>
+          {item.veterinarieName}
+        </Text>
+
+        <View style={styles.infoRow}>
+          <Ionicons name="map-outline" size={14} color={ColorPalette.grey} />
+          <Text text70 color={ColorPalette.grey} marginL-10>
+            {item.street}, {item.locality}, {item.city} {item.cp}
+          </Text>
+        </View>
+
+        <View style={styles.infoRow}>
+          <Ionicons name="call-outline" size={14} color={ColorPalette.grey} />
+          <Text text80 color={ColorPalette.grey} marginL-10>
+            {item.phone}
+          </Text>
+        </View>
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardStyles: {
+    shadowColor: "black",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  infoAreaStyles: {
+    flex: 1,
+    marginLeft: 20,
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+});
+
+export default CardVeterinary;
